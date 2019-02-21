2316d2bc093ee25a1de9f73e2c25a175a4ac887f
16220218 <16220218@cs12214.comp.hkbu.edu.hk>
2019-02-21 16:16:51 +0800

    initial commit

---
+++ api/models/User.js
@@ -0,0 +1,56 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    username: {
+      type: "string",
+      unique: true,
+      required: true
+    },
+    password: {
+      type: "string"
+    },
+
+    role: {
+      type: 'string',
+      // enum: ['admin', 'tester', 'visitor'],
+      isIn: ['admin', 'tester', 'visitor'],
+      defaultsTo: 'visitor'
+    },
+
+    supervises: {
+      collection: 'Person',
+      via: 'worksFor'
+    },
+
+  },
+
+  customToJSON: function () {
+    // Return a shallow copy of this record with the password removed.
+    return _.omit(this, ['password'])
+  },
+
+
+};
+
